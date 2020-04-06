192aae36ff48a85cbef02ce4da594b56c23a0178
Bogdan Dev <bbkk.developer@gmail.com>
2020-04-06 21:08:08 +0300

    added migration for created passports table

---
+++ database/migrations/2020_04_06_164743_create_passports_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePassportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passports', function (Blueprint $table) {
+            $table->id();
+            $table->integer('agent_id');
+            $table->string('serie');
+            $table->string('passport_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('passports');
+    }
+}

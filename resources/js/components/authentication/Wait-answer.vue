c7828e30445b105af140145c50e0ee6cb29c5043
Bogdan-t <user332145@gmail.com>
2020-04-08 20:54:52 +0300

    change form varification, background

--- resources/js/components/authentication/Wait-answer.vue
+++ resources/js/components/authentication/Wait-answer.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="app">
     <div class="label__block">
+      <div class="rhombus-1"></div>
+      <div class="rhombus-2"></div>
+      <div class="rhombus-3"></div>
         <div class="label__block-wrapper">
-          <div class="label__logo-wrapper">
-            <div class="label__logo"></div>
-          </div>
-          <div class="label__hr"></div>
-          <div class="label__header-wrapper">
-            <h1 class="label__header">
-              <alert-svg class="label__header-svg"></alert-svg>
-              Ваше звернення в обробці
-            </h1>
-          </div>
-          <h1 class="label__subheader">Чекайте</h1>
-          <p class="label__paragraph">
-            Ми зв'яжемося з вами, найближчим часом
-          </p>
+        <div class="label__logo-wrapper">
+          <div class="label__logo"></div>
         </div>
+        <div class="label__hr"></div>
+        <div class="label__header-wrapper">
+          <h1 class="label__header">
+            <alert-svg class="label__header-svg"></alert-svg>
+            Ваше звернення в обробці
+          </h1>
+        </div>
+        <h1 class="label__subheader">Чекайте</h1>
+        <p class="label__paragraph">
+          Ми зв'яжемося з вами, найближчим часом
+        </p>
+      </div>
     </div>
   </div>
 </template>

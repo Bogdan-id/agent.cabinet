aa7089147a5b22c95e2a4059faffeff0d498bf08
Bogdan Dev <bbkk.developer@gmail.com>
2020-04-09 21:13:03 +0300

    created active user middleware

---
+++ app/Http/Middleware/ActiveUser.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+class ActiveUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(Auth::user()){
+             $user = User::find(Auth::user()->id);
+       
+            if($user->is_active === 0){
+                return redirect('verification');
+            }
+            
+            return $next($request);
+        }else{
+            return $next($request);
+        }
+       
+        
+    }
+}

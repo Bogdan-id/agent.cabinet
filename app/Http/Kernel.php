0a419e777c90bc0d8dc523c6b5cf1abc6b4136a8
Bogdan Dev <bbkk.developer@gmail.com>
2020-04-10 03:40:27 +0300

    register new midlleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\ActiveUser::class,
+        \App\Http\Middleware\DontActiveUser::class,
     ];
 
     /**
@@ -64,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'user_active' => \App\Http\Middleware\ActiveUser::class,
+        'dont_user_active' => \App\Http\Middleware\DontActiveUser::class,
     ];
 }

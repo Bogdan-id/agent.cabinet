31f841dc66a97a5be142fb5ef983e3a19e97d23d
Bogdan Dev <bbkk.developer@gmail.com>
2020-04-14 23:47:36 +0300

    created calculate client

---
+++ app/Http/Clients/CalculateClient.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vitalijmihalisin
+ * Date: 2019-06-02
+ * Time: 22:35
+ */
+
+namespace App\Http\Clients;
+
+
+use GuzzleHttp\Client;
+
+class CalculateClient
+{
+    protected $client;
+    protected $api_key;
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => env('CALC_API_URL')
+        ]);
+
+        $this->api_key = env('CALC_API_KEY');
+    }
+
+    public function calc() {
+        return '/api/leasing/test.json';
+    }
+
+    public function runCalculate($data) : array
+    {
+        $response = $this->client
+            ->post('/api/leasing/test.json', [
+                'query' => [
+                    'authkey' => $this->api_key
+                ],
+                'json' => $data
+
+            ])
+            ->getBody()
+            ->getContents();
+
+        return json_decode($response, true);
+    }
+}

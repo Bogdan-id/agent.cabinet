accf12eafa7fd38ec89befae8e4b38ce8b50f98a
Bogdan-t <user332145@gmail.com>
2020-04-18 20:59:58 +0300

    add footer navigation

--- resources/js/components/agent-cabinet/layout/dashboard/ContentFooter.vue
+++ resources/js/components/agent-cabinet/layout/dashboard/ContentFooter.vue
@@ -1,6 +1,28 @@
 <template>
-  <footer class="footer">
-    <div class="container-fluid d-flex flex-wrap justify-content-between ">
+  <footer class="footer flex-column">
+    <div :class="`footer__nav justify-space-between pa-3 ${xSmall ? 'flex-column align-center' : 'flex-row'}`">
+      <bestLLogo :width="'140px'" :height="'40px'" :customStyle="'margin-bottom: 10px;'"/>
+      <div class="footer__contacts">
+        <ul class="footer__contacts-wrapper">
+          <li class="contact-title">
+            <v-icon class="icon-position" small v-text="'mdi-map-marker'"></v-icon>
+            01054, м.Киив, вулиця ЯРОСЛАВИВ ВАЛ, будинок 13/2
+          </li>
+          <li class="contact-title">
+            <v-icon class="icon-position" small v-text="'mdi-email'"></v-icon>
+            <a href="#">office@bestleasing.com.ua</a>
+          </li>
+          <li class="contact-title">
+            <v-icon class="icon-position" small v-text="'mdi-cellphone-iphone'"></v-icon>
+            +38 (044) 359 03 99
+          </li>
+        </ul>
+      </div>
+      <div class="footer__feedback">
+        <a class="footer__custom-btn">Зворотнiй зв`язок</a>
+      </div>
+    </div>
+    <div class="container-fluid d-flex flex-wrap justify-content-between pt-2 pb-2">
       <div>
         <router-link class="--dark" :to="{path:'/home'}">Dashboard</router-link>
       </div>
@@ -11,7 +33,57 @@
   </footer>
 </template>
 <script>
-export default {};
+import bestLLogo from '../../assets/svg-icons/best-leasing-logo.vue'
+
+export default {
+  components: {
+    bestLLogo,
+  },
+  computed: {
+    xSmall() {
+      return this.$vuetify.breakpoint.xs
+    }
+  },
+}
 </script>
-<style>
+
+<style lang="scss">
+  .footer {
+    .footer__nav {
+      width: 100%;
+      border-bottom: 1px solid #cdcdcd;
+      display: flex;
+    }
+    .footer__contacts-wrapper {
+      list-style: none;
+      .contact-title {
+        position: relative;
+        padding: 1px 0;
+        .icon-position {
+          position: absolute; 
+          top: 4px; 
+          left: -20px;
+        }
+      }
+    }
+    .footer__custom-btn {
+      transition: all 0.25s ease;
+      white-space: pre;
+      color: white;
+      background: #f44336;
+      font-size: 13px;
+      line-height: 30px;
+      display: block;
+      padding: 0 13px;
+      &:hover {
+        color: white;
+        background: #df473d;
+        box-shadow: 0px 7px 13px -6px rgba(230,101,101,1);
+      }
+    }
+    .footer__feedback {
+      display: flex;
+      align-items: center;
+    }
+  }
 </style>

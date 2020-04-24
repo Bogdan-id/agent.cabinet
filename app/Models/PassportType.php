2f3656aee7bbfd31f9c5ee632f1effd58b314cc7
Bogdan Dev <bbkk.developer@gmail.com>
2020-04-24 17:31:33 +0300

    added empty model variables

--- app/Models/PassportType.php
+++ app/Models/PassportType.php
@@ -7,4 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class PassportType extends Model
 {
     public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        //
+    ];
+
+
+     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+      //
+    ];
+
+    protected $casts = [
+        //
+    ];
 }

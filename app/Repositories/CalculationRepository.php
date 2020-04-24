cfef324fe3f016cc23f3542e1677584591ca5eab
Bogdan Dev <bbkk.developer@gmail.com>
2020-04-24 17:32:28 +0300

    write to db without json_encode

--- app/Repositories/CalculationRepository.php
+++ app/Repositories/CalculationRepository.php
@@ -27,8 +27,8 @@ class CalculationRepository extends BaseRepository implements CalculationReposit
     {
         $this->model->agent_id = $attributes['request_data']['agent_id'];
         $this->model->request_id = $attributes['result_data']['requestId'];
-        $this->model->result_data = json_encode($attributes['result_data']);
-        $this->model->request_data = json_encode($attributes['request_data']);
+        $this->model->result_data = $attributes['result_data'];
+        $this->model->request_data = $attributes['request_data'];
         $this->model->save();
 
         return $this->model;

e04b3ee6393f5692bf519ee5f7bf6b8d0fa20c4b
Bogdan Dev <bbkk.developer@gmail.com>
2020-04-28 20:06:48 +0300

    added variables to LeasingRequest model

--- app/Models/LeasingRequest.php
+++ app/Models/LeasingRequest.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class LeasingRequest extends Model
 {
-    //
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        //
+    ];
+
+
+     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+      //
+    ];
+
+    protected $casts = [
+        //
+    ];
 }

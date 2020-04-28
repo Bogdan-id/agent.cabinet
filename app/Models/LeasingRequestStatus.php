9d4f00113b3644b7ad545014f7114aeb695b8120
Bogdan Dev <bbkk.developer@gmail.com>
2020-04-28 20:07:10 +0300

    added variables to LeasingRequestStatus model

--- app/Models/LeasingRequestStatus.php
+++ app/Models/LeasingRequestStatus.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class LeasingRequestStatus extends Model
 {
-    //
+    public $timestamps = false;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        //
+    ];
+
+
+     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+      //
+    ];
+
+    protected $casts = [
+        //
+    ];
 }

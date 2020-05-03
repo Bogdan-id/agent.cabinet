2d5b0fdbac9d0f6f56f463352504d7504618b76d
Bogdan Dev <bbkk.developer@gmail.com>
2020-05-03 20:58:21 +0300

    added method for creating useful material category

--- app/Http/Controllers/UsefulMaterialsController.php
+++ app/Http/Controllers/UsefulMaterialsController.php
@@ -3,8 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\{
+    UsefulMaterialsCategory
+};
+use Illuminate\Support\Str;
 
 class UsefulMaterialsController extends Controller
 {
-    //
+    public function __construct()
+    {
+        //$this->middleware('auth');
+    }
+
+    public function getCategories()
+    {
+        //
+    }
+
+    public function createCategory(Request $request)
+    {
+        $data = $request->post();
+        $usefulMaterialsCategory = new UsefulMaterialsCategory;
+        $usefulMaterialsCategory->name = $data['name'];
+        $usefulMaterialsCategory->slug = Str::slug($data['name'], '-');
+        $usefulMaterialsCategory->save();
+
+        return response()->json($usefulMaterialsCategory);
+    }
+
+
+    public function updateCategory($id)
+    {
+        //
+    }
+
+    public function destroyCategory($id)
+    {
+        //
+    }
 }

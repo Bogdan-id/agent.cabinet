1ec06a9bc40814f73039750da28b0aacb913ccbe
Bogdan Dev <bbkk.developer@gmail.com>
2020-05-27 08:41:40 +0300

    create admin ManagersController with update method

---
+++ app/Admin/Controllers/ManagersController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\{
+    Manager,
+};
+use App\User;
+use App\Http\Clients\BitrixClient;
+
+class ManagersController extends Controller
+{
+    public function updateManagers(BitrixClient $bitrixClient)
+    {
+        $managers = $bitrixClient->getManagers();
+        foreach($managers as $manager){
+            $managerExists = Manager::where('bitrix_id', '=', $manager['ID'])->first();
+            if(!$managerExists){
+                $managerModel = new Manager;
+                $managerModel->name = "{$manager['NAME']} {$manager['LAST_NAME']} {$manager['SECOND_NAME']}";
+                $managerModel->email = $manager['EMAIL'];
+                $managerModel->phone = $manager['WORK_PHONE'] ?? 'Номер не указан';
+                $managerModel->bitrix_id = $manager['ID'];
+                $managerModel->save();
+            }
+        }
+
+        return response()->json([
+            'status' => 200
+        ]);
+    }
+}

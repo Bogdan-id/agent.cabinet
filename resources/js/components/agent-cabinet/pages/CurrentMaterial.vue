46033882ae05b0be597d0cc32b250edf059be225
Bogdan-t <user332145@gmail.com>
2020-06-26 14:11:30 +0300

    added formated data to material

--- resources/js/components/agent-cabinet/pages/CurrentMaterial.vue
+++ resources/js/components/agent-cabinet/pages/CurrentMaterial.vue
@@ -8,7 +8,7 @@
       <v-divider class="ml-4 mr-4 mb-1"></v-divider>
       <div style="padding: 0 25px; text-align: left;">
         <span style="letter-spacing: 0.05rem; font-size: 0.79rem; display: inline-block;">
-          <!-- {{`${article.updated_at.substring(0,10).replace(/-/g, '/')}`}}&nbsp; {{`${article.updated_at.substring(12,19)}`}} -->
+          {{`${article.updated_at.substring(0,10).replace(/-/g, '/')}`}}&nbsp; {{`${article.updated_at.substring(12,19)}`}}
         </span>
       </div>
       <v-card-text v-html="article.content" class="current-material" style="overflow: hidden;">

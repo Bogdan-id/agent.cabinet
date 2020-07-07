5ca9f61e368b284f675b85b94d7878c91459da14
Bogdan Dev <bbkk.developer@gmail.com>
2020-07-08 01:33:46 +0300

    created AgentCommisionController

---
+++ app/Admin/Controllers/AgentCommisionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\{
+    Agent,
+    IdCard,
+    Passport
+};
+use App\Mail\{
+    AgentAcceptMail,
+    AgentRejectMail
+};
+use Mail;
+use App\User;
+
+class AgentCommisionController extends Controller
+{
+    //
+}

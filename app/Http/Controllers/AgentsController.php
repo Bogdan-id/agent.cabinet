500ba146d27f7d9b3943b00ef0cdab84fc91069f
MacPro <macpro@MacBook-Pro-MacPro.local>
2020-07-13 18:47:17 +0300

    fixed bug

--- app/Http/Controllers/AgentsController.php
+++ app/Http/Controllers/AgentsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\{
     AgentCreateRequest,
-    AgentUpdateRequest,
+    AgentUpdateRequest
 };
 use App\User;
 use App\Models\{

64efac72b51a5c012bdb52c49c1796747ee97d19
Bogdan-t <user332145@gmail.com>
2020-07-14 14:12:34 +0300

    fixed color icon in sidebar

--- resources/js/components/agent-cabinet/assets/svg-icons/rewardRequests.vue
+++ resources/js/components/agent-cabinet/assets/svg-icons/rewardRequests.vue
@@ -1,6 +1,6 @@
 <template>
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 512.005 512.005" style="width: 24px; height: 24px;" xml:space="preserve">
+	 viewBox="0 0 512.005 512.005" style="width: 24px; height: 24px;  fill: black;" xml:space="preserve">
 <g>
 	<g>
 		<g>

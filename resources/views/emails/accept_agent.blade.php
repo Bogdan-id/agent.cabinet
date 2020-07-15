bfbc6fcfc486fbb55ca8cc85e58dd988a4154320
Bogdan-t <user332145@gmail.com>
2020-07-15 13:22:04 +0300

    added email to accept registration

--- resources/views/emails/accept_agent.blade.php
+++ resources/views/emails/accept_agent.blade.php
@@ -1,1 +1,45 @@
-Ваша заявка принята
+<html>
+  <head>
+    <style>
+    </style>
+  </head>
+  <body style="font-family: Helvetica, Tahoma, sans-serif; margin: 0; padding: 0;">
+    <center>
+      <table style="background: #d34a44; width: 450px; padding: 0 15px;">
+        <tr>
+          <td style="border-bottom: 2px solid white;">
+            <img style="width: 170px; padding: 15px 0" src="../../../public/images/white-logo-risized.png" />
+          </td>
+        </tr>
+        <tr>
+          <td style="color: white; padding: 10px 0; font-size: 0.85rem">БIЛЬШЕ НIЖ ЛIЗИНГ</td>
+        </tr>
+      </table>
+      <table cellpadding="15" style="width: 450px; margin: 25px auto 0 auto;" >
+        <tr>
+          <td align="center" style="letter-spacing: 0.03rem;">Дякуємо що приєдналися до команди агентiв <brBEST Leasing!</td>
+        </tr>
+        <tr>
+          <td align="center" style="letter-spacing: 0.03rem;">Вiдтепер вам доступнi вci переваги спiвпрацi з, мабуть, найкращою лiзинговою компанiєю України!</td>
+        </tr>
+        <tr>
+          <td align="center" style="letter-spacing: 0.03rem;">З нами все просто - рекомендуй нас Клiєнтам, отримуй високу фiнансову винагороду, реалiзуй свої мрiї!</td>
+        </tr>
+        <tr>
+          <td align="center"><button style="border: none; outline: none; background: #d34a44; color: white; padding: 13px 25px; font-size: 1rem; font-weight: bold; border-radius: 5px; letter-spacing: 0.06rem; cursor: pointer;">Доступ до кабiнету</button></td>
+        </tr>
+        <tr>
+          <td align="center">
+            <div style="font-size: 0.89rem; font-weight: bold">Ваш директор  з продажiв - Нечаєв В.Е.</div>
+          </td>
+        </tr>
+        <tr>
+          <td style="background:none; border: solid 1px #d34a44; border-width:3px 0 0 0; height:1px; width:100%; margin:0px 10px 0px 10px; padding-top: 0;padding-bottom: 0;">&nbsp;</td>
+        </tr>
+      </table>
+      <a href="https://www.instagram.com/bestleasing.ua/"><img src="../../../public/images/instagram-test2.png" width="65" height="65" /></a>
+      <a href="https://www.facebook.com/bestleasingua/"><img src="../../../public/images/facebook.png" width="65" height="65" /></a>
+      <a href="https://bestleasing.com.ua/ua/"><img src="../../../public/images/512best-leasing.png" width="65" height="65" /></a>
+    </center>
+  </body>
+</html>

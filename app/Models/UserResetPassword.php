4d1f250144518532c7e463512ad0122c890c0afe
MacPro <macpro@MacBook-Pro-MacPro.local>
2020-07-17 17:46:05 +0300

    created UserResetPassword model

---
+++ app/Models/UserResetPassword.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserResetPassword extends Model
+{
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'is_active'
+    ];
+
+
+     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+      //
+    ];
+
+    protected $casts = [
+        //
+    ];
+}

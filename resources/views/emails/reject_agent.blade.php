f0fd84d7e4dd7627c45e243d592b1f9cc89422e4
Bogdan-t <user332145@gmail.com>
2020-07-27 10:26:09 +0300

    added email temlate letter to reject agent registration

--- resources/views/emails/reject_agent.blade.php
+++ resources/views/emails/reject_agent.blade.php
@@ -1,1 +1,48 @@
-Ваша заявка отклонена
+<html>
+  <head>
+    <style>
+    </style>
+    <meta charset="UTF-8">
+  </head>
+  <body style="font-family: Helvetica, Tahoma, sans-serif; margin: 0; padding: 0;">
+    <center>
+      <table style="background: #d34a44; width: 450px; padding: 0 15px;">
+        <tr>
+          <td style="border-bottom: 2px solid white;">
+            <img 
+              alt="Logo" 
+              title="Logo"
+              style="width: 170px; padding: 15px 0; display: block; " 
+              src="https://i.ibb.co/kSCPfS0/white-logo-risized.png" />
+          </td>
+        </tr>
+        <tr>
+          <td style="color: white; padding: 10px 0; font-size: 0.85rem">БIЛЬШЕ НIЖ ЛIЗИНГ</td>
+        </tr>
+      </table>
+      <table cellpadding="15" style="width: 450px; margin: 25px auto 0 auto; color: black;" >
+        <tr>
+          <td align="center" style="letter-spacing: 0.03rem;">Нажаль вам вiдмовлено у реєстрації <br />BEST Leasing</td>
+        </tr>
+        <tr>
+          <td align="center" style="letter-spacing: 0.03rem;">Lorem ipsum dolor sit amet, consectetuer adipiscing elit!</td>
+        </tr>
+        <tr>
+          <td align="center" style="letter-spacing: 0.03rem;">Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.</td>
+        </tr>
+        
+        <tr>
+          <td align="center">
+            <div style="font-size: 0.89rem; font-weight: bold">BEST Leasing</div>
+          </td>
+        </tr>
+        <tr>
+          <td style="background:none; border: solid 1px #d34a44; border-width:3px 0 0 0; height:1px; width:100%; margin:0px 10px 0px 10px; padding-top: 0;padding-bottom: 0;">&nbsp;</td>
+        </tr>
+      </table>
+      <a target="_blank" href="https://www.instagram.com/bestleasing.ua/"><img  style="display: inline-block;" alt="instagram logo" title="instagram logo" src="https://i.ibb.co/gVFNrs4/instagram.png" width="65" height="65"  /></a>
+      <a target="_blank" href="https://www.facebook.com/bestleasingua/"><img style="display: inline-block;" alt="facebook logo" title="facebook logo"  src="https://i.ibb.co/RhFwYgk/facebook.png" width="65" height="65"  /></a>
+      <a target="_blank" href="https://bestleasing.com.ua/ua/"><img src="https://i.ibb.co/87LmmDR/best-leasing.png" style="display: inline-block" alt="best-leasing logo" title="best-leasing logo" width="65" height="65"  /></a>
+    </center>
+  </body>
+</html>

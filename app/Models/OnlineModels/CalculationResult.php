eaf8095fb22e6a4cc0dba305703236f74c0eb6ff
MacPro <macpro@MacBook-Pro-MacPro.local>
2020-07-28 19:02:32 +0300

    added attributes to CalculationResult model

--- app/Models/OnlineModels/CalculationResult.php
+++ app/Models/OnlineModels/CalculationResult.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class CalculationResult extends Model
 {
-    //
+    /**
+     * The connection name for the model.
+     *
+     * @var string
+     */
+    protected $connection = 'mysql_online';
+
+    protected $table = 'calculation_result';
+
+    public $timestamp = true;
+
+    protected $fillable = [
+        'request_id',
+        'request_data',
+        'result_data',
+        'type',
+        'bitrix_type',
+        'is_sent',
+    ];
 }

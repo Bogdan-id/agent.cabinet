fb30712051767334de40aad8dfe1b36924796e02
MacPro <macpro@MacBook-Pro-MacPro.local>
2020-07-28 19:03:33 +0300

    added attributes to FormOption model

--- app/Models/OnlineModels/FormOption.php
+++ app/Models/OnlineModels/FormOption.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class FormOption extends Model
 {
-    //
+    /**
+     * The connection name for the model.
+     *
+     * @var string
+     */
+    protected $connection = 'mysql_online';
+
+    protected $table = 'form_option';
 }

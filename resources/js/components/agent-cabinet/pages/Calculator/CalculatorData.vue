11e0bb93f39ba61666d34e961dd3918783e4697c
Bogdan-t <user332145@gmail.com>
2020-08-03 14:14:39 +0300

    added row leasin object type. Column car changed to model

--- resources/js/components/agent-cabinet/pages/Calculator/CalculatorData.vue
+++ resources/js/components/agent-cabinet/pages/Calculator/CalculatorData.vue
@@ -533,6 +533,9 @@
             {{ item.created_at.substring(0, 10) }}
           </span>
         </template>
+        <template v-slot:item.leasingObjectType="{ item }">
+          <span>{{ item.request_data.leasingObjectType.label}}</span>
+        </template>
         <template v-slot:item.actions="{ item }">
           <div style="display: flex; justify-content: center">
             <v-tooltip bottom>
@@ -543,7 +546,7 @@
                     v-on="on"
                     icon>
                     <v-icon
-                      color="red darken-2"
+                      color="grey darken-2"
                       >
                       mdi-pencil-outline
                     </v-icon>
@@ -560,7 +563,7 @@
                     v-on="on"
                     icon>
                     <v-icon
-                      color="red darken-2"
+                      color="grey darken-2"
                       >
                       mdi-file-find-outline
                     </v-icon>
@@ -577,7 +580,7 @@
                     v-on="on"
                     icon>
                     <v-icon
-                      color="red darken-2">
+                      color="grey darken-2">
                       mdi-file-document-edit-outline
                     </v-icon>
                   </v-btn>
@@ -593,7 +596,7 @@
                     v-on="on"
                     icon>
                     <v-icon
-                      color="red darken-2"
+                      color="grey darken-2"
                       >
                       mdi-delete-forever-outline
                     </v-icon>
@@ -733,11 +736,12 @@ export default {
 
     tableHeader: [
       { text: 'Код розрахунку', value: 'request_id', align: 'start'},
+      { text: 'Дата', value: 'created_at', align: 'center' },
+      { text: 'Тип ПЛ', value: 'leasingObjectType', align: 'center' },
       { text: 'Марка', value: 'request_data.leasedAssertMark.name', align: 'center'},
-      { text: 'Авто', value: 'request_data.leasedAssertModel.name', align: 'center' },
+      { text: 'Модель', value: 'request_data.leasedAssertModel.name', align: 'center' },
       { text: 'Цiна, грн', value: 'request_data', align: 'center' },
       // { text: 'АВ, %', value: 'sendGraph', align: 'center' },
-      { text: 'Дата', value: 'created_at', align: 'center' },
       { text: 'Дiї', value: 'actions', align: 'center', sortable: false },
     ],
     tabledata: [],
@@ -1297,6 +1301,7 @@ export default {
             this.loading = false
             if(response.data.length > 0)  {
               this.tabledata = response.data
+              console.log(this.tabledata)
               this.$store.commit('addGraph', response.data)
             } else {
               this.tabledata = []

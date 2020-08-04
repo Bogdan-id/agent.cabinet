0031a13516904a9fa4c1f22a41567b3ed4b559c2
Bogdan-t <user332145@gmail.com>
2020-08-04 17:53:37 +0300

    added $nextTick to change active tab in charts

--- resources/js/components/agent-cabinet/pages/Charts/ChartDiagram.vue
+++ resources/js/components/agent-cabinet/pages/Charts/ChartDiagram.vue
@@ -769,7 +769,7 @@ export default {
     },
     changeActive() {
       let tabs = document.querySelectorAll('#chart-diagram .tabs-input')
-      setTimeout(() => {
+      this.$nextTick(() => {
         tabs.forEach(element => element.classList.remove('active'))
         let getValue = document.querySelectorAll('#chart-diagram .tabs-input input')
         getValue.forEach(val => {
@@ -777,7 +777,7 @@ export default {
             val.parentNode.classList.add('active')
           }
         })
-      }, 150)
+      })
     },
     switchGraphName(graph) {
       if(graph === 'annuity' || graph === 'Ануїтет') {return '1'}

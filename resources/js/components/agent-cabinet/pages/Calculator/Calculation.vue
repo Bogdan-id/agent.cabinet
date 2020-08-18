975f92c6b5e9eb80df07762fd8f5930550f959d5
Bogdan-t <user332145@gmail.com>
2020-08-18 10:37:50 +0300

    fixed availability to choose bus leasing object type

--- resources/js/components/agent-cabinet/pages/Calculator/Calculation.vue
+++ resources/js/components/agent-cabinet/pages/Calculator/Calculation.vue
@@ -67,7 +67,6 @@
             class="radio-objectType"
             id="bus"
             name="leasing-type"
-            checked
             value="Автобуси">
           <label for="bus" id="Автобуси" :class="mediumAndDown ? 'leasing-type-block small' : 'leasing-type-block'">
             <bus :width="mediumAndDown ? '40' : '47'" :heiht="mediumAndDown ? '18' : '21'" class="leasing-type-icon"></bus>

073be79e83d7f81328cd0954d75a9c2bfb37f649
MacPro <macpro@MacBook-Pro-MacPro.local>
2020-08-20 18:08:11 +0300

    fix

--- app/Http/Requests/CalculateRequest.php
+++ app/Http/Requests/CalculateRequest.php
@@ -48,7 +48,7 @@ class CalculateRequest extends JsonRequest
             'leasingObjectType.label' => 'required|string',
 
             'leasedAssertMark' => 'required|array',
-            'leasedAssertMark.name' => 'required|string|nullable',
+            'leasedAssertMark.name' => 'string|nullable',
             'leasedAssertMark.value' => 'required|integer',
 
             'leasedAssertModel' => 'required|array',
